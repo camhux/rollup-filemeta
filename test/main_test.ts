ed183de013d203419c4c7704c4485c6ef294fa7b
Cameron Martin <camzig@gmail.com>
2024-11-01 13:31:42 -0700

    Deno fmt

--- test/main_test.ts
+++ test/main_test.ts
@@ -1,19 +1,22 @@
-import * as path from 'node:path';
-import {rollup} from 'rollup';
-import plugin from '../src/index.ts';
+import * as path from "node:path";
+import { rollup } from "rollup";
+import plugin from "../src/index.ts";
 import { assertEquals } from "@std/assert";
 
-Deno.test('simple filename transform', async () => {
-	const source = path.resolve(import.meta.dirname ?? './test', 'fixtures/simple.js');
+Deno.test("simple filename transform", async () => {
+  const source = path.resolve(
+    import.meta.dirname ?? "./test",
+    "fixtures/simple.js",
+  );
 
-	const bundle = await rollup({
-		input: source,
-		plugins: [plugin()],
-	});
+  const bundle = await rollup({
+    input: source,
+    plugins: [plugin()],
+  });
 
-	const code = await bundle.generate({
-		compact: true,
-	});
+  const code = await bundle.generate({
+    compact: true,
+  });
 
-	assertEquals(code.output[0].code, `console.log('simple.js');`);
+  assertEquals(code.output[0].code, `console.log('simple.js');`);
 });
